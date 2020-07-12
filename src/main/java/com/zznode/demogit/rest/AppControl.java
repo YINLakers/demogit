e81be413b36b5d4af64ad908909ba71441a085cb
YINLakers <1320207218@qq.com>
2020-07-12 19:35:55 +0800

    demogit项目第一次提交

---
+++ src/main/java/com/zznode/demogit/rest/AppControl.java
@@ -0,0 +1,34 @@
+package com.zznode.demogit.rest;
+
+import java.util.HashMap;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+	/**
+	 * spring-boot-demo-2-1
+	 * 
+	 * @author wujing
+	 */
+	@RestController
+	@RequestMapping(value = "/index")
+	public class AppControl {
+
+		@RequestMapping
+		public String index() {
+			return "hello world";
+		}
+
+		// @RequestParam 简单类型的绑定，可以出来get和post
+		@RequestMapping(value = "/get")
+		public HashMap<String, Object> get(@RequestParam String name) {
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("title", "hello world");
+			map.put("name", name);
+			return map;
+		}
+
+
+	}
---
+++ src/test/java/com/zznode/demogit/DemogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zznode.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
