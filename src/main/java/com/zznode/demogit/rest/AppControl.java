2b5b076a5fbbb08e34756456fbf6a75f70099737
YINLakers <1320207218@qq.com>
2020-07-12 20:31:12 +0800

    查询修改

--- src/main/java/com/zznode/demogit/rest/AppControl.java
+++ src/main/java/com/zznode/demogit/rest/AppControl.java
@@ -1,41 +1,54 @@
 package com.zznode.demogit.rest;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
+import org.assertj.core.util.DateUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * spring-boot-demo-2-1
+ * 
+ * @author wujing
+ */
+@RestController
+@RequestMapping(value = "/index")
+public class AppControl {
 
-	/**
-	 * spring-boot-demo-2-1
-	 * 
-	 * @author wujing
-	 */
-	@RestController
-	@RequestMapping(value = "/index")
-	public class AppControl {
-
-		@RequestMapping
-		public String index() {
-			return "hello world";
-		}
-
-		// @RequestParam 简单类型的绑定，可以出来get和post
-		@RequestMapping(value = "/get")
-		public HashMap<String, Object> get(@RequestParam String name) {
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			map.put("title", "hello world");
-			map.put("name", name);
-			return map;
-		}
-
-		
-		@RequestMapping(value = "/getAge")
-		public HashMap<String, Object> getAge() {
-			HashMap<String, Object> map = new HashMap<String, Object>();
-			map.put("Born", "19930118");
-			return map;
-		}
-
+	@RequestMapping
+	public String index() {
+		return "hello world";
 	}
+
+	// @RequestParam 简单类型的绑定，可以出来get和post
+	@RequestMapping(value = "/get")
+	public HashMap<String, Object> get(@RequestParam String name) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("title", "hello world");
+		map.put("name", name);
+		return map;
+	}
+
+	@RequestMapping(value = "/getAge")
+	public HashMap<String, Object> getAge() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		SimpleDateFormat df = new SimpleDateFormat("yyyy");
+		Date date = new Date();
+
+		map.put("Born", "19930118");
+		map.put("Age", Integer.parseInt(df.format(date)) - 1993);
+		return map;
+	}
+
+	public static void main(String[] args) {
+
+		SimpleDateFormat df = new SimpleDateFormat("yyyy");
+		Date date = new Date();
+
+		System.out.println(Integer.parseInt(df.format(date)) - 1993);
+	}
+
+}
